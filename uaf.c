26525234a0d829331f3038af857372ec54df775a
Parisa Ettehad <parisa98ettehad@gmail.com>
2026-04-27 18:50:52 -0400

    message

--- uaf.c
+++ uaf.c
@@ -1,36 +1,31 @@
 void *malloc(unsigned long size);
 void free(void *ptr);
 
-
-void use(int *p) {
+void sink(int *p) {
     *p = 42;
 }
 
-void bad() {
+void bad_uaf() {
     int *v1 = (int *)malloc(5 * sizeof(int));
     free(v1);
-    use(v1);   // ❌ UAF
+    sink(v1);   
 }
 
-void good() {
-    int *v1 = (int *)malloc(5 * sizeof(int));
-    use(v1);
-    free(v1);  // ✅ safe
-}
 
 void good_different_pointer() {
     int *v1 = (int *)malloc(5 * sizeof(int));
     int *v2 = (int *)malloc(5 * sizeof(int));
 
     free(v1);
-    use(v2);   // ✅ safe (different pointer)
+    sink(v2);   
     free(v2);
 }
+
 
 void good_stack_pointer() {
     int x = 5;
     int *v1 = (int *)malloc(5 * sizeof(int));
 
     free(v1);
-    use(&x);   // ✅ safe (not heap pointer)
+    sink(&x);   
 }
